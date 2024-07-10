e72fa3361397d744f59cefc69e1a6523800026b4
Chetan9850 <chetancdalawaimath@gmail.com>
2024-07-10 16:46:39 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,27 +1,33 @@
-#include <ctype.h>
+// StringCalculator.c
 
-int sumOfDigits(int s){
-	int sum = 0;
-	if (s<=1000){
-		while(s){
-			sum = sum + (s%10);
-			s/=10;
-		}
-	}
-	printf("sumOfDigits result: %d\n", sum); // Debug statement
-	return sum;
-}
+#include "StringCalculator.h"
+#include <stdlib.h>
+#include <string.h>
 
-int add(const char *str){
-	int i=0, s=0, sum = 0;
-	while(str[i]){
-		while(isdigit(str[i++])){
-			s = (s*10) + str[i-1]-'0';
-		}
-		printf("s before sumOfDigits: %d\n", s); // Debug statement
-		sum = sum + sumOfDigits(s);
-		printf("sum after sumOfDigits: %d\n", sum); // Debug statement
-		s=0;
-	}
-	return sum;
+int add(const char* input) {
+    if (input == NULL || strcmp(input, "") == 0) {
+        return 0;
+    }
+
+    int sum = 0;
+    char delimiter = ',';
+    const char* numbers = input;
+
+    // Handle custom delimiter
+    if (input[0] == '/' && input[1] == '/') {
+        delimiter = input[2];
+        numbers = input + 4; // Skip "//[delimiter]\n"
+    }
+
+    // Tokenize the input string
+    char* token = strtok((char*)numbers, ",\n");
+    while (token != NULL) {
+        int num = atoi(token);
+        if (num <= 1000) {
+            sum += num;
+        }
+        token = strtok(NULL, ",\n");
+    }
+
+    return sum;
 }
