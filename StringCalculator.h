5c7f6697bf2f5429f62bbf5e82e3b959ddd9337a
Chetan9850 <chetancdalawaimath@gmail.com>
2024-07-11 10:33:09 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,30 +4,125 @@
 #include <stdlib.h>
 #include <string.h>
 
-int add(const char* input) {
-    if (input == NULL || strcmp(input, "") == 0) {
+int add_arr(int *arr,int cnt)
+{
+    int i = 0;
+    int total = 0;
+    while(i < cnt)
+    {
+        if(arr[i] < 1000)
+        {
+          total += arr[i];
+        }
+        i++;
+    }
+
+    return total;
+}
+
+
+
+int check_num(const char ch)
+{
+    if(ch >= '0' && ch <= '9')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void strtonum(const char* ch, int *num_arr,int *cnt)
+{
+    static int num = 0;
+
+    if(check_num(ch[*cnt]))
+    {
+       num = num * 10 + ch[*cnt] - '0';
+       if(check_num(ch[(*cnt)+1]))
+       {
+           (*cnt)++;
+       }
+       else
+       {
+          num_arr[*cnt] = num;
+          num = 0;
+          (*cnt)++;
+       }
+    }
+    else
+    {
+        (*cnt)++;
+        num = 0;
+    }
+}
+
+int sum(const char *ch)
+{
+    int cnt =0;
+    int total = 0;
+    int num_arr[100] = {0};
+    while(ch[cnt] != '\0')
+    {
+       strtonum(ch,num_arr,&cnt);
+    }
+
+    total = add_arr(num_arr,cnt);
+
+    return total;
+}
+
+
+int neg_loop(const char *ch,int i)
+{
+      if(ch[i] == '-')
+      {
+          if(check_num(ch[i+1]))
+          {
+              return 0;
+          }
+      }
+    return 1;
+}
+
+int check_neg(const char *ch)
+{
+    int res = 0,i=0;
+    while(ch[i] != '\0')
+    {
+      res = neg_loop(ch,i);
+      if(res == 0)
+      {
+          break;
+      }
+      i++;
+    }
+    return res;
+}
+
+int check_string(const char *ch)
+{
+    if(ch != NULL)
+    {
+       if(check_neg(ch))
+       {
+           return 1;
+       }
+    }
+
+    return 0;
+}
+int add(const char *ch)
+{
+    if(check_string(ch))
+    {
+        int total = sum(ch);
+
+        if(total > 0)
+        {
+            return total;
+        }
         return 0;
     }
 
-    int sum = 0;
-    char delimiter = ',';
-    const char* numbers = input;
-
-    // Handle custom delimiter
-    if (input[0] == '/' && input[1] == '/') {
-        delimiter = input[2];
-        numbers = input + 4; // Skip "//[delimiter]\n"
-    }
-
-    // Tokenize the input string
-    char* token = strtok((char*)numbers, ",\n");
-    while (token != NULL) {
-        int num = atoi(token);
-        if (num <= 1000) {
-            sum += num;
-        }
-        token = strtok(NULL, ",\n");
-    }
-
-    return sum;
+    return -1;
 }
